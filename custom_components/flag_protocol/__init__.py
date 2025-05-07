e3a86691b6e91a09fafc9535fbbff80a8b4c2493
svbui <50503484+svbui@users.noreply.github.com>
2025-05-07 19:07:43 +0200

    Create __init__.py
    Initial file

---
+++ custom_components/flag_protocol/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    country = entry.data["country"]  # "nl" or "be" etc.
+    module = importlib.import_module(f".flag_rules.{country}", package=__name__)
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        "get_flag_status": module.get_flag_status,
+        "get_next_flag_day": module.get_next_flag_day,
+    }
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True

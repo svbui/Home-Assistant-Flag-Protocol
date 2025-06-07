2a945657245066a84cf983ec022f256b1bf3e0a6
svbui <50503484+svbui@users.noreply.github.com>
2025-06-07 18:18:56 +0200

    Update const.py
    Binary Sensor added

--- custom_components/flag_protocol/const.py
+++ custom_components/flag_protocol/const.py
@@ -1,4 +1,13 @@
 DOMAIN = "flag_protocol"
+
+from .flag_rules import nl as country_nl
+from .flag_rules import be as country_be
+from .flag_rules import se as country_se
+from .flag_rules import dk as country_dk
+from .flag_rules import no as country_no
+from .flag_rules import fi as country_fi
+from .flag_rules import is_ as country_is
+
 COUNTRIES = {
     "be": "Belgium",
     "dk": "Denmark",
@@ -7,4 +16,13 @@ COUNTRIES = {
     "nl": "Netherlands",
     "no": "Norway",
     "se": "Sweden",
+}
+COUNTRY_MODULES = {
+    "nl": country_nl,
+    "be": country_be,
+    "se": country_se,
+    "dk": country_dk,
+    "no": country_no,
+    "fi": country_fi,
+    "is": country_is,
 }

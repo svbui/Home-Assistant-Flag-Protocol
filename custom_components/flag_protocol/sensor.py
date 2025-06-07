ad4bce8854ead5dc16bc1d31ced3fb698cac3f7a
svbui <50503484+svbui@users.noreply.github.com>
2025-06-07 18:18:35 +0200

    Update sensor.py
    Binary Sensor added

--- custom_components/flag_protocol/sensor.py
+++ custom_components/flag_protocol/sensor.py
@@ -6,7 +6,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import dt as dt_util
 
-from .const import COUNTRIES, DOMAIN
+from .const import COUNTRY_MODULES, COUNTRIES, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
